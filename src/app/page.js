8fe11698f1598ae304e18212d8a13ee416c618be
Caleb <chite2457@gmail.com>
2025-03-30 11:02:12 -0500

    add wallet loading

--- src/app/page.js
+++ src/app/page.js
@@ -153,8 +153,9 @@ export default function Home() {
         </div>
       </div>
         :
-
-        <p>Connect a Wallet to Continue!</p>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
       }
       
     </div>
